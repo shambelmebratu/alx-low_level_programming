527db90012f95b705229f50c0915eaa9b0c36674
shambi21 <shambi2008@gmail.com>
2021-08-26 18:27:49 +0300

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-	/**  print programming is like building a multilinual puzzle
+	/**
+	 *print programming is like building a multilinual puzzle
 	 *main- function is the executed function 
 	 */
 	puts("\"Programming is like building a multilingual puzzle");
