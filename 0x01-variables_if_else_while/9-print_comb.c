1d1c332c227678536f3097a3d2da2801acd3f29c
shambi21 <shambi2008@gmail.com>
2021-08-27 18:20:06 +0300

    print 0 to 9 separete by comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,7 +11,13 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		putchar(i + ", " + '0');
+		if (i != 9)
+		{
+			putchar(i + '0')
+			putchar(", ")
+		}
+		else
+			putchar(i);
 	}
 	putchar('\n');
 
