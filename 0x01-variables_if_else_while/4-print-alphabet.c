6c19e23a4895db1d7ae1b5718adaac0a32d1effe
shambi21 <shambi2008@gmail.com>
2021-08-27 14:38:58 +0300

    a to z except q and e

---
+++ 0x01-variables_if_else_while/4-print-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main function
+ * Description: print a to z except q and e by using break
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' or 'e')
+			break;
+		putchar(ch);
+	}
+	putchar('\n');
+
+return (0);
+}
