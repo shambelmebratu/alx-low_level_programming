edf9b9fe18ed8704ef89ed65d4b60cf6b53e996c
shambi21 <shambi2008@gmail.com>
2021-08-27 18:12:11 +0300

    print 0 to 9 separete by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Description: print 0 to 9 and ,
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		puthchar(',')
+	}
+	putchar('\n');
+
+return (0);
+}
