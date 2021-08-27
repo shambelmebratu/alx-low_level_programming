6ef8dbba892741138f42b73959a4c4b2fa2dbf78
shambi21 <shambi2008@gmail.com>
2021-08-27 14:59:10 +0300

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main function
+ * Description: print numbers from 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (1 = 0; ch <= 9; i++)
+	{
+		putchar(1);
+	}
+	putchar('\n');
+
+return (0);
+}
