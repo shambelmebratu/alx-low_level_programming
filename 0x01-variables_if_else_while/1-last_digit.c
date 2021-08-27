74ffbdca9cb9f8b7b4e5280ef037798e8a0581df
shambi21 <shambi2008@gmail.com>
2021-08-27 13:38:52 +0300

    print last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - main function
+ * Description: print positive and negative value by using if statement
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -12,20 +16,19 @@ int main(void)
 	int lastdigit = n % 10;
 	if (n<0)
 	{
-	    lastdigit=-lastdigit;
+		lastdigit=-lastdigit;
 	}
-    
 	if (lastdigit > 5)
 	{
-	    printf("last digit of %d is %d and is greater than 5\n", n, lastdigit);
+		printf("last digit of %d is %d and is greater than 5\n", n, lastdigit);
 	}
 	else if (lastdigit == 0)
 	{
-	    printf("last digit of %d is %d and is 0\n", n, lastdigit);
+		printf("last digit of %d is %d and is 0\n", n, lastdigit);
 	}
 	else if (lastdigit < 5 && lastdigit != 0)
 	{
-	    printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 	}
 	return (0);
 }
