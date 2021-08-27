79e6a8bce1872da561d7f0d6846d55783992ae25
shambi21 <shambi2008@gmail.com>
2021-08-27 11:32:16 +0300

    print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+  
+int main()  
+{  
+    char ch = 'a';  
+  
+    printf("Lowercase English Alphabets:\n");  
+    while(ch <= 'z')  
+    {  
+        printf("%c", ch);  
+        ch++;  
+    }  
+    printf("\n");  
+  
+    return 0;  
+} 
