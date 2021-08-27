87961b7683faa3991a7653f0b24191c774d4cedf
shambi21 <shambi2008@gmail.com>
2021-08-27 13:34:06 +0300

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,8 +17,8 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-    	}
-    	else if (n == 0)
+	}
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
@@ -27,4 +27,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
-}	
+}
