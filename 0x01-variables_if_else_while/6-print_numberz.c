151c5c9d29377e1428a916de38efa2c4c982d1dd
shambi21 <shambi2008@gmail.com>
2021-08-27 15:13:10 +0300

    print number by using puthchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Description: print a to z except q and e by using break
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; ch <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+return (0);
+}
