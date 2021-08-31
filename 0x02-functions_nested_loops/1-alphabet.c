5dd7b9189be99abe7b4bf705360402a73e33bc2c
shambi21 <shambi2008@gmail.com>
2021-08-31 19:16:08 +0300

    print lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/** 
+ * print_alphabet : print lowercase alphabet 
+ * Description:print lowercase alphabet by using putchar,
+ * Return: void 
+ * */
+
+void print_alphabet(void)
+{ 
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');}
