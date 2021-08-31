be35b2337a7784eaa102cfb7d4253cff794c9f4b
shambi21 <shambi2008@gmail.com>
2021-08-31 21:13:06 +0300

    add two number

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/** 
+/**
  * add - Adds two values
  * @n1: first argument
  * @n2: second argument
- * Return: Integer value 
+ * Return: Integer value
  */
 int add(int n1, int n2)
 {
