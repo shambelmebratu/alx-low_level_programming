e57efb17a5a48e9510a2b5037774a41f897dfc62
shambi21 <shambi2008@gmail.com>
2021-08-31 17:13:36 +0300

    print holberton using putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,8 +8,13 @@
 
 int main(void)
 {
-	char *h="Holberton";
-	while (*h)
-		putchar(*s++);
-	return (0);
+	char str[] = "_putchar\n";
+	int i = 0; 
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+		_putchar(c);
+		i++;
+	}	
+return (0);
 }
