89b74bd5e68e5b1b1f647da467f9a0c7b7c823af
shambi21 <shambi2008@gmail.com>
2021-08-31 23:04:11 +0300

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,29 +5,31 @@
  */
 void times_table(void)
 {
-	int x, y, z, u, d;
-	for (x = 0; x <= 9; x++)
+	int i, j, k, m, d;
+
+	for (i = 0; i <= 9; i++)
 	{
-		for (y = 0; y <= 9; y++)
+		for (j = 0; j <= 9; j++)
 		{
-			z = x * y; if (z > 9)
+			k = i * j; 
+			if (k > 9)
 			{
-				u = z % 10;
-				d = (z - u) / 10;
+				m = k % 10;
+				d = (k - m) / 10;
 				_putchar(44);
 				_putchar(32);
 				_putchar(d + '0');
-				_putchar(u + '0');
+				_putchar(m + '0');
 			}
 			else
 			{
-				if (y != 0)
+				if (j != 0)
 				{
 					_putchar(44);
 					_putchar(32);
 					_putchar(32);
 				}
-				_putchar(z + '0');
+				_putchar(k + '0');
 			}
 		}
 		_putchar('\n');
