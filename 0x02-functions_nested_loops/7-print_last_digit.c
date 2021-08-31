7fb489b65a9b43a286db59353eece7f103579c9c
shambi21 <shambi2008@gmail.com>
2021-08-31 20:46:04 +0300

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - prototype
+ * Description:checks if a character is lowercase or not
+ * Return: 1 if true. 0 if false.
+ * @c: parameter of int value
+ */
+int _islower(int c)
+{
+	int lastD = c % 10;
+
+	if (n < 0)
+	{
+		lastD = lastD * -1;
+	}
+	_putchar(lastD + '0');
+	return (lastD);
+}
+
