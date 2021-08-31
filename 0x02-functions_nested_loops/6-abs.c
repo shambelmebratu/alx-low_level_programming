ae879da6ec67b09b4280f9cf5f452600097a1613
shambi21 <shambi2008@gmail.com>
2021-08-31 20:40:14 +0300

    print abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - prototype
+ * Description:checks if a character is lowercase or not
+ * Return: 1 if true. 0 if false.
+ * @n: parameter of int value
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n <0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+
+}
