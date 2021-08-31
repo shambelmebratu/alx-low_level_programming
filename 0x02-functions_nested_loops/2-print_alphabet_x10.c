90eaeee52c84a6bf689ea9f9069efcc93aff509e
shambi21 <shambi2008@gmail.com>
2021-08-31 19:45:41 +0300

    print lowercase alphabet 10X

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowercase alphabet
+ * Description:print lowercase alphabet 10 X by using putchar
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+	int i = 0;
+
+	while (i <= 9)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		i++;
+	}	
+}
