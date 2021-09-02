e877f05313c16e2f964425965809aad96aa2c8ec
shambi21 <shambi2008@gmail.com>
2021-09-02 20:34:38 +0300

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - print square #
+ * @n: number of #
+ * Return: void
+ */
+
+void print_square(int n)
+{
+	int i = 0, j=0;
+
+	while (i < n && n > 0)
+	{
+		while (j < n)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
