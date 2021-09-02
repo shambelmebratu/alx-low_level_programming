cad50334d1fbea5f2c3732693e3a0f9fdb99166e
shambi21 <shambi2008@gmail.com>
2021-09-02 20:09:05 +0300

    more nubmers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_numbers - print  0 - 9
+ * more_numbers - print  0 - 9
  *
  * Return: void
  */
 
-void print_numbers(void)
+void more_numbers(void)
 {
 	char a = 0;
 	for (int i = 0; i < 10; i++)
