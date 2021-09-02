9df8b4904143eadfbf90ea2df779c92352d7d9ed
shambi21 <shambi2008@gmail.com>
2021-09-02 16:36:03 +0300

    check if it is uper or lower

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
  * _isupper - function to checks if a character is uppercase
  * @c: argument int value
  * Return: 1 if true, 0 if false */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
