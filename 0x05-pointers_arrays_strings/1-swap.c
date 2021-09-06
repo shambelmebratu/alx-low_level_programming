03a4bdfa8d3abb3b3a23bc3b941d434e700b065c
shambi21 <shambi2008@gmail.com>
2021-09-06 12:44:22 +0300

    swap to number

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,12 +3,13 @@
 /**
  * swap_int - swap a and b
  * @a: argument of pointer a value
- * @b; argument of pointer b value
+ * @b: argument of pointer b value
  * Return: empty
  */
 void swap_int(int *a, int *b)
 {
 	int tempN;
+
 	tempN = *a;
 	*a = *b;
 	*b = tempN;
