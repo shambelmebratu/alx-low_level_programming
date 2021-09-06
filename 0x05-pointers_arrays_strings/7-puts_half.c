588035527b167380ca89963276c94051f2f2f05a
shambi21 <shambi2008@gmail.com>
2021-09-06 21:49:44 +0300

    pirnt second half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - print string function
+ * @str: argument of pointer str value
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
