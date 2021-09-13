18625d090d94d07e9e1f2d6c8924315166656029
shambi21 <shambi2008@gmail.com>
2021-09-13 16:21:40 +0300

    locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locate a char. in a string
+ * @s: the string 
+ * @c: the chx.
+ * Return: return if c found or not
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
