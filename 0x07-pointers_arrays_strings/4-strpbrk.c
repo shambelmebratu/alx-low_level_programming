674c5fd672c983d24fa3c4f6cd9bbd091b30224f
shambi21 <shambi2008@gmail.com>
2021-09-13 18:09:23 +0300

    searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Search a string
+ * @s: string
+ * @accept: set of byte
+ *
+ * Return: check matched or not
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	while(*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+
+		s++;
+	}
+	return ('\0');
+}
