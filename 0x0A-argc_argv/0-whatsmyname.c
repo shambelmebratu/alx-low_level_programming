21610f09d91c2994e62b509afc0576efe85ef0e4
shambi21 <shambi2008@gmail.com>
2021-09-17 16:07:06 +0300

    print my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - main function to print name of the program
+ * @argc: number of command line
+ * @argv: array of char
+ * Return: 0
+ */
+
 int main(int argc, char* argv[])
 {
 	printf("%s\n", argv[argc]);
+
+	return (0);
 }
