9d8e0f743a3ae6e97025af3029da26eb46147566
shambi21 <shambi2008@gmail.com>
2021-09-18 14:09:20 +0300

    mul of 2 argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - main function to print multiplication of 2 argument
+ * @argc: Number of command line
+ * @argv: Array of char
+ * Return: 1 or 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+		j *= atoi(argv[i]);
+
+		printf("%d\n", j);
+	}
+
+	return (0);
+}
