320a1ebe296aa67770584e275245ff8ec09c3a4e
shambi21 <shambi2008@gmail.com>
2021-09-18 13:31:58 +0300

    print the number of argument passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function to print name of the program
+ * @argc: number of command line
+ * @argv: array of char
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+
+	return (0);
+}
